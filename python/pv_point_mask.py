29f48b143751eada594515b4cb26bf145021a6f0
Bohan Zhang <bohan.zhang.1@slu.edu>
2024-03-25 21:20:17 -0500

    Refactored code in pv_point_mask.py to check for non-empty inside_surface and outside_surface before setting cell_mask values. Reshaped binary_mask to 3D grid dimensions for better visualization. Updated clip_box parameters in inside_clip to [0, 100, 0, 100, 25, 50]. Added opacity to mesh visualization in Plotter.

--- python/pv_point_mask.py
+++ python/pv_point_mask.py
@@ -15,8 +15,11 @@ def mask_mesh_by_surface(grid_image_data, mesh_surface):
 
     # Create a mask for the cells in the mesh
     grid["cell_mask"] = np.zeros(grid.n_cells, dtype=int)
-    grid["cell_mask"][inside_surface["cell_ids"]] = 1  # Cells inside the surface
-    grid["cell_mask"][outside_surface["cell_ids"]] = 2  # Cells outside the surface
+    if inside_surface.n_cells > 0:
+        grid["cell_mask"][inside_surface["cell_ids"]] = 1  # Cells inside the surface
+    if outside_surface.n_cells > 0:
+        grid["cell_mask"][outside_surface["cell_ids"]] = 2
+
 
     # Use the implicit distance to create a mask for the points in the mesh
     inside_point_ids = np.argwhere(grid["implicit_distance"] >= 0)
@@ -27,8 +30,8 @@ def mask_mesh_by_surface(grid_image_data, mesh_surface):
 
     return grid
 
-
-stl = pv.STLReader("openFoamCase/constant/geometry/combined.stl").read()
+file_name= "openFoamCase/constant/geometry/combined.stl"
+stl = pv.STLReader(file_name).read()
 grid = pv.ImageData()
 grid.origin = (0, 0, 0)
 # Cell sizes
@@ -47,26 +50,17 @@ masked["cool_math"] = np.zeros(grid.n_points)  # Need to preallocate
 masked["cool_math"][ids] = dist
 
 distance_mask = masked["cool_math"]
-print(f"shape of distance mask: {distance_mask.shape}")
-
-# p = pv.Plotter()
-# p.add_mesh(a, scalars="cool_math")
-# p.show()
-
-# convert to binary mask
-#
-print(f"cell mask shape: {masked['cell_mask'].shape}")
-
 masked["binary_mask"] = np.zeros(masked.n_points, dtype=int)
 masked["binary_mask"][distance_mask > 1] = 0
 masked["binary_mask"][distance_mask <= 1] = 1
 
-# remove all 0s
-
-
 binary_mask = masked["binary_mask"]
 
 print(f"Binary mask shape: {binary_mask.shape}, 1s: {np.sum(binary_mask == 1)}, 0s: {np.sum(binary_mask == 0)}")
+
+# reshape to 3d
+binary_mask = binary_mask.reshape(grid.dimensions)
+print(f"reshaped binary mask shape: {binary_mask.shape}, 1s: {np.sum(binary_mask == 1)}, 0s: {np.sum(binary_mask == 0)}")
 
 a = masked.threshold(1.5, scalars="cell_mask", invert=False)
 a["binary_mask"] = np.zeros(a.n_points, dtype=int)
@@ -75,12 +69,12 @@ a["binary_mask"][a["cool_math"] <= 0] = 0
 print(
     f"Threshold mask shape: {a['binary_mask'].shape}, 1s: {np.sum(a['binary_mask'] == 1)}, 0s: {np.sum(a['binary_mask'] == 0)}")
 
-inside_clip = masked.clip_box([0, 50, 0, 50, 0, 50])
+inside_clip = masked.clip_box([0, 100, 0, 100, 25, 50])
 
 p = pv.Plotter()
-# p.add_mesh(a, scalars="binary_mask", opacity=0.5)
+p.add_mesh(a, scalars="binary_mask", opacity=0.5)
 # plot masked, only 1s
-p.add_mesh(inside_clip, scalars="binary_mask", opacity=0.5, style="points")
+#p.add_mesh(inside_clip, scalars="binary_mask", opacity=0.5, style="points")
 # clip view
 p.view_isometric()
 p.show()
