1067ad3e4a6d637057d6237e75b05d5662f37cc6
Bohan Zhang <bohan.zhang.1@slu.edu>
2024-04-09 13:11:37 -0500

    Clean up

--- python/slide_5k_training_data_visualize.py
+++ python/slide_5k_training_data_visualize.py
@@ -11,7 +11,7 @@ def visualize_data_from_csv(csv_file_path):
     df = pd.read_csv(csv_file_path)
 
     # Filter the data to include only rows where bm is 1
-    filtered_df = df[df['bm'] == 0]
+    filtered_df = df[df['bm'] == 1]
 
     # Create a figure for 3 subplots (3 views)
     fig = plt.figure(figsize=(18, 6))
@@ -49,6 +49,6 @@ def visualize_data_from_csv(csv_file_path):
 
 # Assuming the CSV file is named 'result_preprocessed_time.csv' and located in the current directory
 time = 50
-open_foam_case_root = "../5k_training_dataset/-25_0_-100_-75_0_10"
+open_foam_case_root = "../5k_training_dataset/0_25_0_25_1_11"
 csv_file_path = 'result_preprocessed_' + str(time) + '.csv'
 visualize_data_from_csv(os.path.join(open_foam_case_root, csv_file_path))
