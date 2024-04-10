b989a7cdcab66146fd07a0d8a5d585064cb3930d
Bohan Zhang <bohan.zhang.1@slu.edu>
2024-04-09 23:12:43 -0500

    Added routes for configuring OpenFOAM case, setting up mesh using binary mask, and checking CFD simulation status.

--- python/cfd_server.py
+++ python/cfd_server.py
@@ -23,6 +23,39 @@ def wind():
     return dummy_wind_data
 
 
+@app.route('/openfoam', methods=['POST'])
+def configure_openfoam_case():
+    """
+    Configure the OpenFOAM case
+    input: wind speed, wind direction, wind type
+    :returns: success message, ready to run the simulation, does not start the simulation, wait for the binary mask
+    """
+    # get boundary vertices from request
+    request_json = request.get_json()
+    print("request_json:", request_json)
+    # TODO: update the openfoam case with the wind data
+
+
+@app.route('/mesh', methods=['POST'])
+def setup_mesh_using_binary_mask():
+    """
+    Setup the mesh using the binary mask
+    :returns: success message, ready to run the simulation
+    """
+    # get binary mask from request
+    request_json = request.get_json()
+    print("request_json:", request_json)
+    # TODO: setup the mesh using the binary mask
+
+@app.route('/cfd', methods=['GET'])
+def cfd_status():
+    """
+    Check the status of the CFD simulation
+    :returns: status of the CFD simulation [running, completed, idle]
+    """
+    # TODO: return the current openfoam case status
+
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0',port=5001)
